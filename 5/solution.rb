d88998dd68652e0a75e51ed6c25ec260888a8d33
Vadim Boltach <vadim@boltach.com>
2022-12-05 01:13:46 -0500

    Day5 part1

---
+++ 5/solution.rb
@@ -0,0 +1,39 @@
+require 'pp'
+
+lines = ARGF.readlines(chomp: true)
+sep = lines.index { |l| l.empty? }
+
+amount_of_stacks = lines[sep - 1].strip[-1].to_i
+commands = lines[sep + 1, lines.length]
+
+stacks = Array.new(amount_of_stacks) { [] } # [["Z", "N"], ["M", "C", "D"], ["P"]]
+
+lines[0, sep - 1].reverse.each do |line|
+    line.split("") # ["    ", "[D] ", "   "]
+        .each_slice(4)
+        .map { |s| s.join.gsub(/[ \[\]]/,'') } # ["", "D", ""]
+        .each_with_index do |stack_item, i|
+            if !stack_item.empty?
+                stacks[i] << stack_item
+            end
+        end    
+end
+
+commands.each do |command|
+    _,how_many_crates,_,from,_,to = command.split(" ")
+
+    how_many_crates.to_i.times do
+       crate = stacks[from.to_i - 1].pop
+       stacks[to.to_i - 1] << crate
+    end
+end
+
+stack_top = stacks.inject([]) do |top, s|
+    if !s.empty?
+        top << s.last
+    end
+end
+
+pp stack_top.join
+
+
