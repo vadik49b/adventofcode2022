2f6a2dd750163f7dd9b1d671ae10432d6274c9c8
Vadim Boltach <vadim@boltach.com>
2022-12-14 00:33:17 -0500

    Day13 part2

--- 13/solution.rb
+++ 13/solution.rb
@@ -3,19 +3,15 @@ require 'pp'
 lines = ARGF.readlines(chomp: true)
 
 def check(left_packet, right_packet)
+    if left_packet.empty? || right_packet.empty?
+        return left_packet.length - right_packet.length != 1
+    end
+
     left = left_packet.shift
     right = right_packet.shift
 
-    if left.nil? && right.nil?
-        return true
-    end
-
-    if left.nil? && !right.nil?
-        return true
-    end
-
-    if !left.nil? && right.nil?
-        return false
+    if left.is_a?(Integer) && right.is_a?(Integer)
+        return left < right
     end
 
     if left.is_a?(Array) && right.is_a?(Array)
@@ -30,24 +26,11 @@ def check(left_packet, right_packet)
         return check(left, [right])
     end
 
-    if left == right
-        return check(left_packet, right_packet)
-    end
-
-    return left < right
+    return check(left_packet, right_packet)    
 end
 
-i = 1
-sum = 0
-lines.each_slice(3) do |packets|
-    left_packet = eval(packets[0])
-    right_packet = eval(packets[1])
+lines = lines.filter { |l| !l.empty? }
+before_divider_1 = lines.filter { |l| check(eval(l), eval("[[2]]")) }
+before_divider_2 = lines.filter { |l| check(eval(l), eval("[[6]]")) }
 
-    if check(left_packet, right_packet)
-        sum += i
-    end
-
-    i += 1
-end
-
-pp sum
+pp (before_divider_1.length + 1) * (before_divider_2.length + 2)
